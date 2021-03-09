2372cbf920460d2ab39ac869da671cc2d0534b53
SoulOfMod <72465476+SoulOfMod@users.noreply.github.com>
2021-03-09 15:40:00 +0100

    jour 2 fini a part bonus 3&4

--- jour-2/Bonus2.js
+++ jour-2/Bonus2.js
@@ -1,39 +1,23 @@
-var month = "June";
+var month = "May";
 
 switch (month) {
     case "January":
-        console.log("Winter");
-        break;
     case "February":
-        console.log("Winter");
-        break;
     case "March":
         console.log("Winter");
         break;
     case "April":
-        console.log("Spring");
-        break;
     case "May":
-        console.log("Spring");
-        break;
     case "June":
         console.log("Spring");
         break;
     case "July":
-        console.log("Summer");
-        break;
     case "August":
-        console.log("Summer");
-        break;
     case "September":
         console.log("Summer");
         break;
     case "October":
-        console.log("Fall");
-        break;
     case "November":
-        console.log("Fall");
-        break;
     case "December":
         console.log("Fall");
         break;
