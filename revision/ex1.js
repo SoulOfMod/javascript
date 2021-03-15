9eeda5adcd7a3f9a2bafff35ae3ce86194737ffe
SoulOfMod <72465476+SoulOfMod@users.noreply.github.com>
2021-03-15 15:45:45 +0100

    revision exo2

--- revision/ex1.js
+++ revision/ex1.js
@@ -5,8 +5,7 @@
 function sortLetters(word) {
     word = word.split('')
     word.sort()
-    console.log(word.join('')
-    )
+    console.log(word.join(''))
 }
 
 sortLetters('konexio')
--- revision/ex2.js
+++ revision/ex2.js
@@ -1,5 +1,44 @@
-/* - Créer une fonction `sortLetters` qui reçoit une string en paramètre
-- La fonction retourne une string avec les mêmes lettres, dans l'ordre alphabétique
-- Appelez votre fonction avec "konexio" comme argument et vérifiez que vous obtenez "eiknoox" */
+/* - Créez une fonction `countEach` qui reçoit une string contenant des x et des o en paramètre
+- La fonction retourne true si il y a autant de x que de o, sinon elle retourne false
+- Vérifiez que l'argument "xxxoooox" déclenche un true, et que "xox" déclenche un false */
 
-function countEach(word)
+function countEach(word) {
+
+    console.log("word", word)
+    xcount = 0
+    ocount = 0
+
+    console.log("word length", word.length)
+
+
+    for (i = 0; i < word.length; i++) {
+
+        console.log("word char at", word.charAt(i))
+        
+        if (word.charAt(i) === "x") {
+            xcount++
+            console.log(xcount)
+        }
+
+        else if (word.charAt(i) === "o") {
+            ocount++
+            console.log(ocount)
+        }
+
+        else {
+
+        }
+    }
+
+    if (xcount === ocount) {
+
+        console.log("true")
+
+    } else {
+
+        console.log("false")
+    }
+
+}
+
+countEach('xooxxoox')
